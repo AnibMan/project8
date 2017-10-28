71f47a8b3e37f531d0a4e6a2379daa010c101b25
Anib Manandhar <Anib Manandhar>
2017-10-28 21:29:39 +0545

    navbar search edit

--- resources/views/mainViews/navbar.blade.php
+++ resources/views/mainViews/navbar.blade.php
@@ -1,36 +1,43 @@
 <nav class="navbar navbar-default navbar-fixed-top" style="text-align: center">
     <div class="container-fluid">
         <div class="navbar-header">
+
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
 
+
+
             <a class="navbar-brand" href="#">WebSiteName</a>
+
         </div>
+        <form class="navbar-form navbar-right">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">
+                        Search
+                    </button>
+
+                </div>
+
+            </div>
+        </form>
         <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
+
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><font color="#20b2aa"> <span class="glyphicon glyphicon-user"></span>  Sign Up </font></a></li>
+                <li><a href="#"><font color="#1e90ff"> <span class="glyphicon glyphicon-log-in"></span> Login</font></a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Page 2</a></li>
                 <li> <!-- search bar -->
-                    <form class="navbar-form navbar-right" style="margin-left: 5%" >
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                            <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit">
-                                    Search
-                                </button>
-
-                            </div>
-
-                        </div>
-                    </form></li>
+                   </li>
             </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
+
         </div>
     </div>
 </nav>
